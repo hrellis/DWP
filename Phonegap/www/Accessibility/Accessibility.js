72a205990017418afd8d2d39ccd8b8784d725b72
Heather Ellis <heather@heatherellis.net>
2013-09-25 12:24:11 +0100

    Fixed the colour theme to work on all pages using local storage

--- Phonegap/www/Accessibility/Accessibility.js
+++ Phonegap/www/Accessibility/Accessibility.js
@@ -8,19 +8,6 @@ script.src = 'http://code.jquery.com/jquery-1.7.1.min.js';
 script.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(script);
 
-
-function changeStyle(ID) {
-	// create cookie to store body style ID in
-	var cookieName = 'bodyStyleID=';
-	var cookieVal = ID;
-	var date = new Date();
-	date.setFullYear(date.getFullYear() + 1);
-	var cookieExpires = ';expires=' + date.toGMTString();
-	document.cookie = cookieName + cookieVal + cookieExpires + ';' + 'path=/;';
-	// set body's ID to match the chosen colour scheme.
-	document.body.id = ID;
-}
-
 // -----------------------------------------------------------------------------------------
 
 function changeFontSize(percentage) {
@@ -64,12 +51,16 @@ function setFont(element, font) {
 }
 // ------------------------------------------------------------------------------------------
 
-function changeTheme(font) {
+function changeTheme(id) {
 	//store the theme in local storage
-	localStorage.theme = theme;
+	localStorage.theme = id;
 	
 	//set the body's theme to the specified one 
-	setTheme();
+	setTheme(id);
+}
+
+function setTheme(theme) {
+	document.body.id = theme;	
 }
 // ------------------------------------------------------------------------------------------
 function restoreStyle(){
@@ -80,6 +71,10 @@ function restoreStyle(){
 	
 	if (localStorage.font != undefined){
 		changeFont(localStorage.font);
+	}
+	
+	if(localStorage.theme != undefined){
+		changeTheme(localStorage.theme);
 	}
 }
 
